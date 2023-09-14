95f405ada58214e24a18da0e01ed3654d10918f2
Sergei Nikhaenko <exsilet@mail.ru>
2023-09-14 19:46:42 +0500

    GameRunner

---
+++ Assets/Scripts/Infrastructure/GameRunner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Infrastructure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        public GameBootstrapper BootstrapperPrefab;
+
+        private void Awake()
+        {
+            var bootstrapper = FindObjectOfType<GameBootstrapper>();
+            
+            if (bootstrapper != null)
+                return;
+
+            Instantiate(BootstrapperPrefab);
+        }
+    }
+}
